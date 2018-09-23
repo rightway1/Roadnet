953b1b65ebf8be8cbbd8d581df5298dfd79a0a70
Colin Wright <colin.wright@thinkwhere.com>
2018-09-23 23:10:01 +0100

    Added qt5 version of RAMP gui files

--- ramp/gui/ramp_edit_linked_polys_ui.py
+++ ramp/gui/ramp_edit_linked_polys_ui.py
@@ -2,64 +2,50 @@
 
 # Form implementation generated from reading ui file 'ramp_edit_linked_polys_ui.ui'
 #
-# Created: Fri Jun 10 11:38:28 2016
-#      by: PyQt4 UI code generator 4.10.4
+# Created by: PyQt5 UI code generator 5.11.2
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_editLinkedPolysDialog(object):
     def setupUi(self, editLinkedPolysDialog):
-        editLinkedPolysDialog.setObjectName(_fromUtf8("editLinkedPolysDialog"))
+        editLinkedPolysDialog.setObjectName("editLinkedPolysDialog")
         editLinkedPolysDialog.setWindowModality(QtCore.Qt.WindowModal)
         editLinkedPolysDialog.resize(250, 267)
         editLinkedPolysDialog.setModal(True)
-        self.gridLayout = QtGui.QGridLayout(editLinkedPolysDialog)
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.linkedPolysLabel = QtGui.QLabel(editLinkedPolysDialog)
-        self.linkedPolysLabel.setObjectName(_fromUtf8("linkedPolysLabel"))
+        self.gridLayout = QtWidgets.QGridLayout(editLinkedPolysDialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.linkedPolysLabel = QtWidgets.QLabel(editLinkedPolysDialog)
+        self.linkedPolysLabel.setObjectName("linkedPolysLabel")
         self.gridLayout.addWidget(self.linkedPolysLabel, 0, 0, 1, 1)
-        self.label = QtGui.QLabel(editLinkedPolysDialog)
+        self.label = QtWidgets.QLabel(editLinkedPolysDialog)
         self.label.setInputMethodHints(QtCore.Qt.ImhNone)
         self.label.setWordWrap(True)
-        self.label.setObjectName(_fromUtf8("label"))
+        self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 2, 0, 1, 2)
-        self.linkedPolysListWidget = QtGui.QListWidget(editLinkedPolysDialog)
-        self.linkedPolysListWidget.setObjectName(_fromUtf8("linkedPolysListWidget"))
+        self.linkedPolysListWidget = QtWidgets.QListWidget(editLinkedPolysDialog)
+        self.linkedPolysListWidget.setObjectName("linkedPolysListWidget")
         self.gridLayout.addWidget(self.linkedPolysListWidget, 1, 0, 1, 2)
-        self.buttonBox = QtGui.QDialogButtonBox(editLinkedPolysDialog)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.buttonBox = QtWidgets.QDialogButtonBox(editLinkedPolysDialog)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
         self.gridLayout.addWidget(self.buttonBox, 3, 0, 1, 1)
 
         self.retranslateUi(editLinkedPolysDialog)
         QtCore.QMetaObject.connectSlotsByName(editLinkedPolysDialog)
 
     def retranslateUi(self, editLinkedPolysDialog):
-        editLinkedPolysDialog.setWindowTitle(_translate("editLinkedPolysDialog", "Edit linked polygons", None))
-        self.linkedPolysLabel.setText(_translate("editLinkedPolysDialog", "Linked polygons:", None))
-        self.label.setText(_translate("editLinkedPolysDialog", "Modify the selection on the map canvas to link/unlink polygons.", None))
+        _translate = QtCore.QCoreApplication.translate
+        editLinkedPolysDialog.setWindowTitle(_translate("editLinkedPolysDialog", "Edit linked polygons"))
+        self.linkedPolysLabel.setText(_translate("editLinkedPolysDialog", "Linked polygons:"))
+        self.label.setText(_translate("editLinkedPolysDialog", "Modify the selection on the map canvas to link/unlink polygons."))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    editLinkedPolysDialog = QtGui.QDialog()
+    app = QtWidgets.QApplication(sys.argv)
+    editLinkedPolysDialog = QtWidgets.QDialog()
     ui = Ui_editLinkedPolysDialog()
     ui.setupUi(editLinkedPolysDialog)
     editLinkedPolysDialog.show()
--- ramp/gui/ramp_length_of_roads_ui.py
+++ ramp/gui/ramp_length_of_roads_ui.py
@@ -2,57 +2,43 @@
 
 # Form implementation generated from reading ui file 'ramp_length_of_roads_ui.ui'
 #
-# Created: Wed May 25 11:35:51 2016
-#      by: PyQt4 UI code generator 4.10.4
+# Created by: PyQt5 UI code generator 5.11.2
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_RampLengthOfRoadsDialog(object):
     def setupUi(self, RampLengthOfRoadsDialog):
-        RampLengthOfRoadsDialog.setObjectName(_fromUtf8("RampLengthOfRoadsDialog"))
+        RampLengthOfRoadsDialog.setObjectName("RampLengthOfRoadsDialog")
         RampLengthOfRoadsDialog.resize(494, 727)
         RampLengthOfRoadsDialog.setSizeGripEnabled(True)
-        self.gridLayout = QtGui.QGridLayout(RampLengthOfRoadsDialog)
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.plainTextEdit = QtGui.QPlainTextEdit(RampLengthOfRoadsDialog)
-        self.plainTextEdit.setObjectName(_fromUtf8("plainTextEdit"))
+        self.gridLayout = QtWidgets.QGridLayout(RampLengthOfRoadsDialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(RampLengthOfRoadsDialog)
+        self.plainTextEdit.setObjectName("plainTextEdit")
         self.gridLayout.addWidget(self.plainTextEdit, 0, 0, 1, 1)
-        self.buttonBox = QtGui.QDialogButtonBox(RampLengthOfRoadsDialog)
+        self.buttonBox = QtWidgets.QDialogButtonBox(RampLengthOfRoadsDialog)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Close|QtGui.QDialogButtonBox.Save)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Close|QtWidgets.QDialogButtonBox.Save)
         self.buttonBox.setCenterButtons(False)
-        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.buttonBox.setObjectName("buttonBox")
         self.gridLayout.addWidget(self.buttonBox, 1, 0, 1, 1)
 
         self.retranslateUi(RampLengthOfRoadsDialog)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), RampLengthOfRoadsDialog.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), RampLengthOfRoadsDialog.reject)
+        self.buttonBox.accepted.connect(RampLengthOfRoadsDialog.accept)
+        self.buttonBox.rejected.connect(RampLengthOfRoadsDialog.reject)
         QtCore.QMetaObject.connectSlotsByName(RampLengthOfRoadsDialog)
 
     def retranslateUi(self, RampLengthOfRoadsDialog):
-        RampLengthOfRoadsDialog.setWindowTitle(_translate("RampLengthOfRoadsDialog", "RAMP - Length of Roads", None))
+        _translate = QtCore.QCoreApplication.translate
+        RampLengthOfRoadsDialog.setWindowTitle(_translate("RampLengthOfRoadsDialog", "RAMP - Length of Roads"))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    RampLengthOfRoadsDialog = QtGui.QDialog()
+    app = QtWidgets.QApplication(sys.argv)
+    RampLengthOfRoadsDialog = QtWidgets.QDialog()
     ui = Ui_RampLengthOfRoadsDialog()
     ui.setupUi(RampLengthOfRoadsDialog)
     RampLengthOfRoadsDialog.show()
